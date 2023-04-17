d4c25c3a25977520870b1b7759287d0172d6a426
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2023-04-17 08:02:20 +0100

    Initial commit

--- src/pages/base_page.jsx
+++ src/pages/base_page.jsx
@@ -1,8 +1,12 @@
 import './base.css'
 import {Header} from "../components/header";
 import {NavigationMenu} from "../components/navigation_menu/navigation_menu";
+import ReactGA from 'react-ga';
 
 export function BasePage(props) {
+
+    ReactGA.initialize('G-3PVZ08E2BZ');
+
     return (
         <>
             <Header></Header>
--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,8 +1,13 @@
 import './home_page.css'
 import {BasePage} from "./base_page";
 import {Hero} from "../components/hero_section";
+import ReactGA from 'react-ga';
+
 
 export function HomePage(props) {
+
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
     return (
         <BasePage>
             <Hero/>
